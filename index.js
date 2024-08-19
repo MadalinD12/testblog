0aeb0a07906f4b4f696abec2591794ebac2431e0
MadalinD12 <Dinumadalin351@yahoo.ro>
2024-08-19 12:54:15 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+//npm init -y
+//npm i bcrypt connect-mongo cookie-parser dotenv ejs express express-ejs-layouts express-session jsonwebtoken method-override mongoose
+
+
+require('dotenv').config();
+
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+const methodOverride= require('method-override');
+const cookieParser=require('cookie-parser');
+const session = require('express-session');
+//const MongoStore= require('connect-mongo');
+
+//const connectDB=require('./server/config/db');
+//const { isActiveRoute }=require('./server/helpers/routeHelpers');
+
+const app= express();
+const PORT= 5000 || process.env.PORT;
+ 
+// //Connect to DB
+// connectDB();
+
+// app.use(express.urlencoded({extended: true}));
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use(methodOverride('_method'));
+app.get('', (req, res)=>{
+    res.send("Hello World!");
+});
+
+// app.use(session({
+//     secret: 'keyboard cat',
+//     resave: false,
+//     saveUninitialized: true,
+//     store: MongoStore.create({
+//          mongoUrl: process.env.mongodb_URI
+//     }),
+//    //cookie: {maxAge: new Date (Date.now() + (3600000) ) }
+// }))
+
+app.use(express.static('public'));
+
+// // Templating Engine
+// app.use(expressLayout);
+// app.set('layout','./layouts/main');
+// app.set('view engine', 'ejs');
+
+//app.locals.isActiveRoute= isActiveRoute;
+
+
+//app.use('/',require('./server/routes/main'));
+//app.use('/',require('./server/routes/admin'));
+
+app.listen(PORT, () => {
+    console.log(`APP listening on port ${PORT}`);
+});
